8cb102fe6ba0bf2b6e1f46ddd5047b6a901eed71
sunjh <1455364690@qq.com>
2019-09-01 11:04:47 +0800

    修改配置

--- meituan/settings.py
+++ meituan/settings.py
@@ -76,8 +76,14 @@ WSGI_APPLICATION = 'meituan.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'meituan',
+        'USER': 'root',
+        'PASSWORD': 'root',
+        'HOST': '182.254.197.194',
+        'PORT': '3306'
     }
 }
 
